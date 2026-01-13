c7935dfcc191e643db97bcf0b65c0e6a0609b07e
Shiven Chandra <137705277+shivenchandra@users.noreply.github.com>
2026-01-14 00:38:20 +0530

    implement navbar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
     return (
-        <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+        <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden bg-red-500 px-5 py-3 text-black ${containerClass}`}>
             {leftIcon}
             <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'>
                 <div>
